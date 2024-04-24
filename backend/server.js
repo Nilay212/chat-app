21a6b8ab2461a52e8ebcb0879e2a2b913abe0ea4
Nilay <pandaynilay@gmail.com>
2024-04-24 15:49:58 +0530

    done changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -9,24 +10,25 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import { app, server } from "./socket/socket.js";
 
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 
-dotenv.config();
 app.use(express.json())  // to parse incomming request with json payload
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
-
-// app.get("/", (req,res) => {
-//     res.send("Hello World!!hjh");
-// })
 
 
 server.listen(PORT, () => {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port : 5173,
-    proxy : {
+  base: "/chat-app",
+  server: {
+    port: 5173,
+    proxy: {
       "/api": {
-        target : "http://localhost:3000",
-      }
-    }
-  }
+        target: "http://localhost:3000",
+      },
+    },
+  },
 })
